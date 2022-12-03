e195b362a3d4cb838702f1f8029e96afbfe424ed
umutyilmaz@yandex.com <umut.yilmaz@iottech.com.tr>
2022-12-03 23:39:53 +0300

    Initial Commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Umut Yılmaz on 3.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
